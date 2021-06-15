8de31b6372b7455b5309821c791fafe5eecdfca8
Rangeur11 <71460465+Rangeur11@users.noreply.github.com>
2021-06-15 12:10:34 +0200

    semi-responsive

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,17 +1,30 @@
 <template>
 
-<div class="nav-scroller py-1 mb-2">
-    <nav class="nav d-flex justify-content-between" style="background-color: #24262b" >
-        <a class="p-2 link-secondary" href="#" id="navbarItem">  <img src="/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24" class="d-inline-block align-text-top">ExpressTaxi</a>
-        <a class="p-2 link-secondary" href="#" id="navbarItem">Reserver un Taxi</a>
-        <a class="p-2 link-secondary" href="#" id="navbarItem">Nos Services</a>
-        <a class="p-2 link-secondary" href="#" id="navbarItem">Nous Contacter</a>
-        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-            <button class="btn btn-primary me-md-2" type="button">Se Connecter</button>
+    <nav class="navbar navbar-expand-lg" style="background-color: #24262b" >
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#" id="navbarItem">  <img src="/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24" class="d-inline-block align-text-top">ExpressTaxi</a>
+            <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fas fa-bars"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarToggler">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" id="navbarItem">Reserver un Taxi</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" id="navbarItem">Nos Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" id="navbarItem">Nous Contacter</a>
+                    </li>
+                </ul>
+                <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                    <button class="btn btn-primary me-md-2" type="button">Se Connecter</button>
+                </div>
+            </div>
         </div>
+
     </nav>
-</div>
- 
 </template>
 
 <script>
@@ -22,7 +35,6 @@
     a {
         color: whitesmoke;
         text-decoration: none;
-        margin: 1% 4%;
     }
    
    h5{
